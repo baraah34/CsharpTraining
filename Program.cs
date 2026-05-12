3eb7ff244d2ef57fa2b479eac06a25ccab00f129
Baraah <alsadibaraah@gmail.com>
2026-05-12 12:31:39 +0400

    declare all variable

--- Program.cs
+++ Program.cs
@@ -1,10 +1,29 @@
-﻿namespace CsharpTraining
+﻿using System;
+
+namespace CsharpTraining
 {
     internal class Program
     {
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Hello, World!");
+            //declare all variable
+            int accountNumber = 0;
+            string holderName = "";
+            double balance = 0.000;
+            bool isActive = false;
+            char accountType = '-';
+            bool isEmployed = false;
+            double salary = 0.000;
+            int creditScore = 0;
+            int age = 0;
+            double deposit = 0.000;
+            double withdrawal = 0.000;
+            double annualRate = 0.000;
+            double avgBalance = 0.000;
+
+
+               
+            }
         }
     }
-}
