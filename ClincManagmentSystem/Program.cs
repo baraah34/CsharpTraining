dc5568009d0086045af7ffd2a177249d45baf4a5
Baraah <alsadibaraah@gmail.com>
2026-05-14 13:55:30 +0400

    all variable declarations

---
+++ ClincManagmentSystem/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ClincManagmentSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Region 1 — System Storage: all variable declarations 
+
+            
+            const int MAX_PATIENTS = 3;
+            const int MAX_DOCTORS = 2;
+            const int MAX_APPOINTMENTS = 3;
+
+            // Patient 
+            string p1Name = ""; int p1Age = 0; string p1Phone = ""; bool p1Active = false;
+            string p2Name = ""; int p2Age = 0; string p2Phone = ""; bool p2Active = false;
+            string p3Name = ""; int p3Age = 0; string p3Phone = ""; bool p3Active = false;
+            int patientCount = 0;
+
+            // Doctor 
+            string d1Name = ""; string d1Spec = ""; double d1Fee = 0; bool d1Active = false;
+            string d2Name = ""; string d2Spec = ""; double d2Fee = 0; bool d2Active = false;
+            int doctorCount = 0;
+
+            // Appointment 
+            string a1Patient = ""; string a1Doctor = ""; string a1Date = ""; string a1Status = ""; bool a1Active = false;
+            string a2Patient = ""; string a2Doctor = ""; string a2Date = ""; string a2Status = ""; bool a2Active = false;
+            string a3Patient = ""; string a3Doctor = ""; string a3Date = ""; string a3Status = ""; bool a3Active = false;
+            int appointmentCount = 0;
+
+
+
+        }
+    }
+}
